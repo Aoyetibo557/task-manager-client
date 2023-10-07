101341f232854ab3d36c115de984d5d77bc92ad1
Anuoluwapo Gabriel Oyetibo <54692909+Aoyetibo557@users.noreply.github.com>
2023-10-06 20:06:39 -0400

    updating the logo

--- components/base-components/icon/icon.tsx
+++ components/base-components/icon/icon.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { IconType } from "react-icons";
+import Link from "next/link";
 
 interface IconProps {
   icon: IconType | any;
   theme?: string;
   size?: "sm" | "md" | "lg";
   clx?: string;
+  link?: string;
 }
 
 export const Icon: React.FC<IconProps> = ({
@@ -13,12 +15,20 @@ export const Icon: React.FC<IconProps> = ({
   theme,
   size = "medium",
   clx,
+  link,
 }) => {
   const themeClass = theme === "light" && "bg-task-light";
   const sizeClass =
     size === "small" ? "w-6" : size === "medium" ? "w-8" : "w-12";
 
-  return (
+  return link ? (
+    <Link href={link}>
+      <div
+        className={`flex flex-row items-center justify-center rounded-full ${themeClass} ${sizeClass} ${clx}`}>
+        <IconComponent />
+      </div>
+    </Link>
+  ) : (
     <div className={`icon ${themeClass} ${sizeClass} ${clx}`}>
       {IconComponent && IconComponent()}
     </div>
